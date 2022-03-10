5858930f57335fdc5fa2bb72a8d1df4fd6c67fcd
waynethebb <wonjun92@gmail.com>
2022-03-11 00:57:07 +1300

    change port

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ import jobsRouter from './jobs/router';
 const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(bodyParser.json());
