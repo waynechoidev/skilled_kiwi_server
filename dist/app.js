52e4039ebc04f57d6bd9cf1672165364b5468c49
waynethebb <wonjun92@gmail.com>
2022-03-12 23:42:27 +1300

    add test page

--- dist/app.js
+++ dist/app.js
@@ -8,10 +8,12 @@ var router_1 = require("./auth/router");
 var mysql_1 = require("./common/db/mysql");
 var router_2 = require("./jobs/router");
 var app = express();
-var port = process.env.PORT || 8080;
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(cors());
+app.get('/', function (req, res) {
+    res.send('connect');
+});
 app.use('/jobs', router_2.default);
 app.use('/auth', router_1.default);
 app.use(function (req, res, next) {
@@ -24,5 +26,5 @@ app.use(function (error, req, res, next) {
 mysql_1.db.getConnection()
     .then(function (c) { return console.log('db loaded'); })
     .catch(function (e) { return console.error(e); });
-app.listen(port);
+app.listen(process.env.PORT || 8080);
 //# sourceMappingURL=app.js.map
